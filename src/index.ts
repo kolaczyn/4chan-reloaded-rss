f8ffce5498d2f13eb7104af2d26cc689b31f09d7
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-08-03 23:02:01 +0200

    initial working version

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,67 @@
-// --- begin package setup ---
-// export { greeter } from './greeter'
-// export const addTwoNumbers = (a: number, b: number) => a + b
-// --- end package setup --
+import axios from 'axios'
+import { Builder } from 'xml2js'
+import fs from 'fs'
 
-// --- begin binsetup ---
+const API_URL = 'https://api.kolaczyn.com/boards'
+const BOARD_TO_DOWNLOAD = 'a'
 
-import { greeter } from './greeter'
-console.log(greeter('world'))
+type Thread = {
+  id: number
+  message: string
+  repliesCount: number
+  createdAt: string | null
+  imageUrl: string | null
+}
+const encodeXml = (s: string) =>
+  s
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/\t/g, '&#x9;')
+    .replace(/\n/g, '&#xA;')
+    .replace(/\r/g, '&#xD;')
 
-// --- end binsetup
+// Convert the JavaScript object to XML
+
+const main = async () => {
+  // fetch threads
+  const { data } = await axios.get<{ threads: Thread[] }>(`${API_URL}/${BOARD_TO_DOWNLOAD}`)
+
+  const xmlBuilder = new Builder({
+    xmldec: { version: '1.0', encoding: 'UTF-8' },
+  })
+
+  const rssFeed = {
+    rss: {
+      $: {
+        'xmlns:atom': 'http://www.w3.org/2005/Atom',
+        version: '2.0',
+      },
+      channel: {
+        title: 'Boards',
+        description: 'Messageboard by kolaczyn',
+        link: 'https://4chan.kolaczyn.com', // Change this to your website URL
+        lastBuildDate: new Date().toUTCString(),
+        item: data.threads.map(item => {
+          return {
+            title: encodeXml(item.message),
+            description: 'Reply count: ' + item.repliesCount,
+            link: `https://4chan.kolaczyn.com/boards/${BOARD_TO_DOWNLOAD}/${item.id}`,
+            pubDate: new Date(item.createdAt ?? '').toUTCString(),
+          }
+        }),
+      },
+    },
+  }
+
+  const xml = xmlBuilder.buildObject(rssFeed)
+
+  fs.writeFile('rss.xml', xml, function (err: any) {
+    if (err) throw err
+    console.log('Saved!')
+  })
+}
+
+main()
