e24146d00b1b0e438d819cd8128c706db5d822f4
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-08-04 18:47:29 +0200

    serve board's threads

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import axios from 'axios'
 import { Builder } from 'xml2js'
-import fs from 'fs'
+import express from 'express'
+import NodeCache from 'node-cache'
 
 const API_URL = 'https://api.kolaczyn.com/boards'
-const BOARD_TO_DOWNLOAD = 'a'
+
+const appCache = new NodeCache({
+  stdTTL: 60,
+  checkperiod: 120,
+})
 
 type Thread = {
   id: number
@@ -12,22 +17,16 @@ type Thread = {
   createdAt: string | null
   imageUrl: string | null
 }
-const encodeXml = (s: string) =>
-  s
-    .replace(/&/g, '&amp;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&apos;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/\t/g, '&#x9;')
-    .replace(/\n/g, '&#xA;')
-    .replace(/\r/g, '&#xD;')
 
-// Convert the JavaScript object to XML
+const generateBoardXml = async (boardSlug: string) => {
+  console.info('making request for ' + boardSlug)
+  const result = await axios
+    .get<{ threads: Thread[]; name: string; slug: string }>(`${API_URL}/${boardSlug}?sortOrder=creationDate`)
+    .catch(_ => null)
 
-const main = async () => {
-  // fetch threads
-  const { data } = await axios.get<{ threads: Thread[] }>(`${API_URL}/${BOARD_TO_DOWNLOAD}`)
+  if (!result) {
+    return null
+  }
 
   const xmlBuilder = new Builder({
     xmldec: { version: '1.0', encoding: 'UTF-8' },
@@ -40,28 +39,48 @@ const main = async () => {
         version: '2.0',
       },
       channel: {
-        title: 'Boards',
+        title: `/${result.data.slug}/ - ${result.data.name}`,
         description: 'Messageboard by kolaczyn',
         link: 'https://4chan.kolaczyn.com', // Change this to your website URL
         lastBuildDate: new Date().toUTCString(),
-        item: data.threads.map(item => {
-          return {
-            title: encodeXml(item.message),
-            description: 'Reply count: ' + item.repliesCount,
-            link: `https://4chan.kolaczyn.com/boards/${BOARD_TO_DOWNLOAD}/${item.id}`,
-            pubDate: new Date(item.createdAt ?? '').toUTCString(),
-          }
-        }),
+        item: result.data.threads.map(item => ({
+          title: item.message,
+          description: 'Reply count: ' + item.repliesCount,
+          link: `https://4chan.kolaczyn.com/boards/${boardSlug}/${item.id}`,
+          pubDate: new Date(item.createdAt ?? '').toUTCString(),
+        })),
       },
     },
   }
 
-  const xml = xmlBuilder.buildObject(rssFeed)
-
-  fs.writeFile('rss.xml', xml, function (err: any) {
-    if (err) throw err
-    console.log('Saved!')
-  })
+  return xmlBuilder.buildObject(rssFeed)
 }
 
-main()
+const app = express()
+
+app.get('/:board', async (req, res) => {
+  const { board } = req.params
+
+  const fromCache = appCache.get(board)
+  if (fromCache !== undefined) {
+    res.set('Content-Type', 'text/xml')
+    return res.send(fromCache)
+  }
+
+  const xml = await generateBoardXml(board)
+
+  appCache.set(board, xml)
+
+  if (!xml) {
+    res.status(404)
+    return res.send('Not found')
+  }
+
+  res.set('Content-Type', 'text/xml')
+  return res.send(xml)
+})
+
+const PORT = process.env.PORT ?? '3000'
+app.listen(PORT, () => {
+  console.info(`Server running on port http://localhost:${PORT}`)
+})
